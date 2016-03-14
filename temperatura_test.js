0bfc96cca1afab54b6e10368c67a2a26b03d17c6
alu0100825503 <alu0100825503@ull.edu.es>
2016-03-14 17:06:15 +0000

    Añadida expectativa para el tipo de Temperatura

--- temperatura_test.js
+++ temperatura_test.js
@@ -3,8 +3,12 @@ var expect = chai.expect;
 describe("Temperatura", function() {
   describe("constructor", function() {
     it("should have a value", function() {
-      var temp = new Temperatura("32", "f");
-      expect(temp.getValor()).to.equal("32");
+      var temp = new Temperatura(32, "f");
+      expect(temp.getValor()).to.equal(32);
+    });
+    it("should have a type", function() {
+      var temp = new Temperatura(32, "f");
+      expect(temp.getTipo()).to.equal("f");
     });
   });
 });
