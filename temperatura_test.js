13783084942a601d99afab25b680b8a34e0f265a
Óscar <alu0100705638@ull.edu.es>
2016-03-14 18:46:01 +0000

    Superadas las expectativas para el método Fahrenheit

--- temperatura_test.js
+++ temperatura_test.js
@@ -32,3 +32,32 @@ describe("Temperatura", function() {
     });
   });
 });
+
+describe("Fahrenheit", function(){
+  describe("constructor", function(){
+    it("should have a value", function() {
+      var fah = new Fahrenheit(32);
+      expect(fah.getValor()).to.equal(32);
+    });
+    it("should have a type", function() {
+      var fah = new Fahrenheit(32);
+      expect(fah.getTipo()).to.equal("f");  
+    });
+    it("should have a name", function() {
+      var fah = new Fahrenheit(32);
+      expect(fah.name).to.equal("Fahrenheit");  
+    });
+  });
+  describe("#toCelsius", function() {
+    it("should convert Fahrenheit to Celsius", function(){
+      var value = new Fahrenheit(32).toCelsius();
+      expect(value).to.equal(0);
+    });  
+  });
+  describe("#toKelvin", function() {
+    it("should convert Fahrenheit to Kelvin", function(){
+      var value = new Fahrenheit(32).toKelvin();
+      expect(value).to.equal(273.15);
+    });  
+  });
+});
